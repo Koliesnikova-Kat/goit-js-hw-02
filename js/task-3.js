916163278c1d11e26e8628d115c5a3ce86a9b853
Kateryna <koliesnikova.kateryna@gmail.com>
2024-07-07 00:06:48 +0300

    v2

--- js/task-3.js
+++ js/task-3.js
@@ -10,3 +10,29 @@ console.log(checkForSpam("Amazing SalE, only tonight!")); // true
 console.log(checkForSpam("Trust me, this is not a spam message")); // true
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
+
+
+
+
+
+
+function checkForSpam(message) {
+  let messageLower = message.toLowerCase();
+  if (messageLower.includes('sale') || messageLower.includes('spam')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
