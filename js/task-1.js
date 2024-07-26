734c7080ba204daa0c8be7a4438690f7babe0e0e
Kateryna <koliesnikova.kateryna@gmail.com>
2024-07-26 23:36:09 +0300

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-
   const totalPrice = quantity * pricePerDroid;
 
   if (totalPrice > customerCredits) {
-  return "Insufficient funds!";
-  }
-  
-  else {
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
 
